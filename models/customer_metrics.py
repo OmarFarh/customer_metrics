255eee518974352c077195f6c457715a4cdc9112
Omar <omar.ahmed01067@gmail.com>
2024-11-12 11:39:23 +0200

    update

--- models/customer_metrics.py
+++ models/customer_metrics.py
@@ -9,7 +9,7 @@ class CustomerMetrics(models.Model):
 
     customer_id = fields.Many2one(comodel_name='res.partner', string='Customer')
     total_sales = fields.Float(compute='_compute_total_sales' , readonly= True)
-    order_count = fields.Integer(compute='_compute_order_count' , readonly= True)
+    order_count = fields.Integer(compute='_compute_total_sales' , readonly= True)
 
     @api.depends('customer_id')
     def _compute_total_sales(self):
@@ -17,17 +17,10 @@ class CustomerMetrics(models.Model):
         for rec in self:
 
             orders = self.env['sale.order'].search([('partner_id', '=' , rec.customer_id.id)]).mapped('amount_total')
+            rec.order_count = len(orders)
             rec.total_sales = sum(orders)
 
-    @api.depends('customer_id')
-    def _compute_order_count(self):
 
-        for rec in self:
-
-            orders = self.env['sale.order'].search_count([('partner_id', '=' , rec.customer_id.id)])
-            rec.order_count = orders
-
-    @api.model
     def get_top_customers(self , *args, **kwargs):
 
         top_customers = self.env['sale.order'].read_group([('state', '=', 'sale')],
@@ -36,7 +29,10 @@ class CustomerMetrics(models.Model):
             orderby='amount_total desc',
             limit=5,)
 
+        result = []
 
         for customer in top_customers:
             count_orders = self.env['sale.order'].search_count([('partner_id' , '=' , customer['partner_id'][0])])
-            print( f"{customer['partner_id'][1]}: {customer['amount_total']} - count order: {count_orders} ")
+            result.append({"name": customer['partner_id'][1] , "amount_total": customer['amount_total'] , "count_order": count_orders})
+
+        return result
